69e49ef9674de06d419810ed288ba4a8f8abee46
luhtonen <luhtonen@gmail.com>
2016-03-12 15:27:15 +0100

    Adding README to movie find project and improving build

--- moviefind/gulpfile.js
+++ moviefind/gulpfile.js
@@ -3,10 +3,16 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
 gulp.task('browserify', function () {
   browserify('./app/main.js')
     .transform('reactify')
     .bundle()
+    .on('error', handleError)
     .pipe(source('main.js'))
     .pipe(gulp.dest('dist/js'));
 });
